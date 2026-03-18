660eadcec4f1171616f479f7010f4c246979e61b
Francesco Piastra <f.piastra@gmail.com>
2026-03-18 18:25:00 +0100

    Fixing import issue in the core/__init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
 from .SCDevice import (SCDevice, getRegister, load_all_devices)
 from .FileWriter import FileWriter
 
-from Utils import (read_token, load_json_config)
+from .Utils import (read_token, load_json_config)
